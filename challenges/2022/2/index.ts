ba538730b579b7b32252d1eb995ab0e20d583721
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2022-12-02 11:01:41 +0100

    fixup! 2022: day 2

--- challenges/2022/2/index.ts
+++ challenges/2022/2/index.ts
@@ -1,81 +1,39 @@
 import getInput from "../../../utils/getInput";
 
+type ElfHand = "A" | "B" | "C";
+type OwnHand = "X" | "Y" | "Z";
+
 const data = getInput("2022", "2")
   .trim()
   .split("\n")
-  .map((lines) => lines.split(" "));
+  .map((lines) => lines.split(" ") as [ElfHand, OwnHand]);
+
+const result = {
+  A: { X: 4, Y: 8, Z: 3 },
+  B: { X: 1, Y: 5, Z: 9 },
+  C: { X: 7, Y: 2, Z: 6 },
+};
 
 const part1 = () => {
-  const calculateHandPoints = (ownHand: "X" | "Y" | "Z") => {
-    return {
-      X: 1,
-      Y: 2,
-      Z: 3,
-    }[ownHand];
-  };
-
-  const game = (elfHand: "A" | "B" | "C", ownHand: "X" | "Y" | "Z"): number => {
-    const pointsMap = {
-      A: 0,
-      B: 1,
-      C: 2,
-      X: 0,
-      Y: 1,
-      Z: 2,
-    };
-
-    if (pointsMap[elfHand] === 2 && pointsMap[ownHand] === 0) {
-      return 6;
-    }
-
-    if (pointsMap[elfHand] === 0 && pointsMap[ownHand] === 2) {
-      return 0;
-    }
-
-    return pointsMap[elfHand] < pointsMap[ownHand]
-      ? 6
-      : pointsMap[elfHand] > pointsMap[ownHand]
-      ? 0
-      : 3;
-  };
-
   return data
-    .map(
-      ([elfHand, ownHand]: any) =>
-        game(elfHand, ownHand) + calculateHandPoints(ownHand)
-    )
-    .reduce((total, gamepoints) => total + gamepoints, 0);
+    .map(([elfHand, ownHand]) => result[elfHand][ownHand])
+    .reduce((total, gamePoints) => total + gamePoints, 0);
 };
 
 const part2 = () => {
-  const calculateHandPoints = (
-    elfHand: "A" | "B" | "C",
-    gameResult: "X" | "Y" | "Z"
-  ) => {
-    const elfHandPoints = { A: 1, B: 2, C: 3 }[elfHand];
-    const points = (adding: any): number =>
-      [1, 2, 3][(elfHandPoints + adding - 1) % 3];
-    return gameResult === "Z"
-      ? points(1)
-      : gameResult === "Y"
-      ? points(0)
-      : points(2);
-  };
-
-  const game = (gameResult: "X" | "Y" | "Z"): number => {
-    return {
-      X: 0,
-      Y: 3,
-      Z: 6,
-    }[gameResult];
+  const transform: { [key in ElfHand]: { [key in OwnHand]: OwnHand } } = {
+    A: { X: "Z", Y: "X", Z: "Y" },
+    B: { X: "X", Y: "Y", Z: "Z" },
+    C: { X: "Y", Y: "Z", Z: "X" },
   };
 
   return data
-    .map(([elfHand, gameResult]: any): number => {
-      return game(gameResult) + calculateHandPoints(elfHand, gameResult);
-    })
-    .reduce((total, gamepoints) => total + gamepoints, 0);
+    .map(([elfHand, ownHand]): [ElfHand, OwnHand] => [
+      elfHand,
+      transform[elfHand][ownHand],
+    ])
+    .map(([elfHand, ownHand]) => result[elfHand][ownHand])
+    .reduce((total, gamePoints) => total + gamePoints, 0);
 };
-
 console.log(`Solution 1: ${part1()}`);
 console.log(`Solution 2: ${part2()}`);
