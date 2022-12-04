3bde62e56ed1ca5eeb753263e9c91d2ac38ab0fc
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2022-12-04 06:43:05 +0100

    fixup! 2022: day 3

--- challenges/2022/3/index.ts
+++ challenges/2022/3/index.ts
@@ -1,104 +1,40 @@
 import getInput from "../../../utils/getInput";
 
-const priority = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+const rucksacks = getInput("2022", "3").trim().split("\n");
 
-// @ts-ignore
-const splitOn = (amount, arr): string[][] => {
-  if (arr.length < amount) return arr;
-  const x = arr.splice(0, amount);
-  return [x, ...splitOn(amount, arr)];
+const recurringItem = ([first, ...rest]: string[][]) => {
+  const find = first.find((item) => rest.some((i) => i.includes(item)));
+
+  if (find) return find;
+
+  throw new Error("No matching symbol found");
 };
-
-const data = getInput("2022", "3").trim().split("\n");
+const priority = (item: string) =>
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(item) + 1;
+const total = (total: number, item: number) => total + item;
 
 const part1 = () => {
-  return data
-    .map((line) => line.split(""))
-    .map((line): [string[], string[]] => {
-      const firstHalf = line.splice(0, line.length / 2);
-      return [firstHalf, line];
-    })
-    .map(([firstHalf, secondHalf]) => {
-      return firstHalf.find((item) => {
-        return secondHalf.includes(item);
-      });
-    })
-    .map((item) => {
-      // @ts-ignore
-      return priority.indexOf(item) + 1;
-    })
-    .reduce((total, item) => total + item, 0);
+  return rucksacks
+    .map((rucksack) => rucksack.split(""))
+    .map((rucksack) => [rucksack.splice(0, rucksack.length / 2), rucksack])
+    .map(recurringItem)
+    .map(priority)
+    .reduce(total, 0);
 };
 
 const part2 = () => {
-  // @ts-ignore
-  return splitOn(3, data)
+  const groupBy3 = ([first, ...rest]: string[][], rucksack: string) =>
+    first.length < 3
+      ? [[...first, rucksack], ...rest]
+      : [[rucksack], first, ...rest];
+
+  return rucksacks
+    .reduce(groupBy3, [[]])
     .map((group) => group.map((elf) => elf.split("")))
-    .map(([firstHalf, secondHalf, third]) => {
-      // @ts-ignore
-      return firstHalf.find((item) => {
-        return secondHalf.includes(item) && third.includes(item);
-      });
-    })
-    .map((item) => {
-      // @ts-ignore
-      return priority.indexOf(item) + 1;
-    })
-    .reduce((total, item) => total + item, 0);
+    .map(recurringItem)
+    .map(priority)
+    .reduce(total, 0);
 };
 
-// console.log(part1());
+console.log(part1());
 console.log(part2());
